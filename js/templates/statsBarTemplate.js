537ffbf742158378dcf4e6eb52fc814aa5429f2d
reazy015 <redshuhart015@gmail.com>
2018-05-09 19:33:12 +0300

    merge

--- js/templates/statsBarTemplate.js
+++
@@ -1,6 +0,0 @@
-export default (gameState) => {
-  return `
-  <ul class="stats">
-    ${gameState.answers.map((answerType) => `<li class="stats__result stats__result--${answerType}"></li>`).join(``)}      
-  </ul>`;
-};
--- js/utils/create-element.js
+++
@@ -1,5 +0,0 @@
-export default (template = ``, tagName = `div`) => {
-  const outer = document.createElement(tagName);
-  outer.innerHTML = template.trim();
-  return outer;
-};
